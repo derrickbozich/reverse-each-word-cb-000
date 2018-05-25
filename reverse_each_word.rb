9d7a121f9fead3a6c35a3461f65015b43952a3a3
derrick b <dbozich1@gmail.com>
2018-05-25 06:12:11 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,13 +1,4 @@
-def reverse_each_word(string)
-  array = string.split(" ")
-  reversed_array = []
-  array.each {|element| reversed_array.push(element.reverse)}
-  reversed_string = reversed_array.join(" ")
-  reversed_string
 
-end
-
-string = "heloo world!"
 
 def reverse_each_word(string)
   array = string.split(" ")
